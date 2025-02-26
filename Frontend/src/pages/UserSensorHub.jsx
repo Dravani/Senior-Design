c7bd7d222a3dd17f05a6f71d3c468d519aebede0
Sam Forstot <92872780+samforstot@users.noreply.github.com>
2025-02-26 15:10:58 -0500

    created sensor hub and added react router dom

---
+++ Frontend/src/pages/UserSensorHub.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const UserSensorHub = () => {
+    return (
+        <div>
+            <h1>Welcome, User</h1>
+            <h2>Active Sensors</h2>
+        </div>
+    );
+}
+
+export default UserSensorHub;
