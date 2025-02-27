4afc142979805bdf8720e290fd0eedb7496215ef
Sam Forstot <92872780+samforstot@users.noreply.github.com>
2025-02-27 12:57:29 -0500

    setting up table

--- Frontend/src/pages/UserSensorHub.jsx
+++ Frontend/src/pages/UserSensorHub.jsx
@@ -5,10 +5,22 @@ import './UserSensorHub.css'
 
 const UserSensorHub = () => {
     return (
-        <div className="dash-title">
-            <h1>Welcome, User!</h1>
-            <h2><span className="underline">Active Sensors</span></h2>
+        <div className="user-sensor-hub">
+            <div className="dash-title">
+                <h1>Welcome, User!</h1>
+                <h2><span className="underline">Active Sensors</span></h2>
+            </div>
+            <div className="sensor-table">
+                <table>
+                    <tr>
+                        <th>Name</th>
+                        <th>Status</th>
+                        <th>Duration</th>
+                    </tr>
+                </table>
+            </div>
         </div>
+
     );
 }
 
