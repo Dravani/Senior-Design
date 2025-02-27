c28727143a418f73f7f4faf98402e8eff94da240
Sam Forstot <92872780+samforstot@users.noreply.github.com>
2025-02-27 13:45:23 -0500

    added projects page

--- Frontend/src/pages/UserProjectHub.jsx
+++ Frontend/src/pages/UserProjectHub.jsx
@@ -9,6 +9,11 @@ const UserProjectHub = () => {
             <div className="dash-title">
                 <h2><span className="underline">Projects</span></h2>
             </div>
+            <div className="project-cards">
+                <div className="card">
+                    <span className="card-title">+</span>
+                </div>
+            </div>
         </div>
     )
 }
