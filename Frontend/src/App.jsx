9ff99e9d506dd3ff7b03e2ba43fe332c8ab1fd27
Sam Forstot <92872780+samforstot@users.noreply.github.com>
2025-02-27 13:30:46 -0500

    added preliminary projects page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,14 +4,30 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // Pages
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
+import UserSensorHub from './pages/UserSensorHub'
+import UserProjectHub from './pages/UserProjectHub';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/" element={<Login />} />
+        <Route 
+          path="/login"
+          element={<Login />} />
+        <Route 
+          path="/signup"
+          element={<SignUp />} />
+        <Route 
+          path="/"
+          element={<Login />} />
+        <Route
+          path="/projects"
+          element={<UserProjectHub/>}
+        />
+        <Route
+          path="/sensors"
+          element={<UserSensorHub/>}
+        />
       </Routes>
     </BrowserRouter>
   );
