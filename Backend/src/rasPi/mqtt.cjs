63c57bc1db0f56a3ab81dd37b5816278a575d0a0
Sam Forstot <92872780+samforstot@users.noreply.github.com>
2025-03-24 20:00:53 -0400

    created basic websocket in UserSensorHub

--- Backend/src/rasPi/mqtt.cjs
+++ Backend/src/rasPi/mqtt.cjs
@@ -1,6 +1,10 @@
 /* Base run for connecting to pi */
 const mqtt = require('mqtt');
 
+/* Basic Web Sockets */
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8080});
+
 const brokerIP = 'mqtt://100.110.144.47'; // Subject to change
 const client = mqtt.connect(brokerIP);
 
@@ -14,6 +18,15 @@ client.on('connect', () => {
 })
 
 client.on('message', (topic, message) =>{
-  const data = message.toString();
-  console.log(`Received message on ${topic}: ${data}`);
-});
+    const data = message.toString();
+    console.log(`Received message on ${topic}: ${data}`);
+  });
+
+wss.on('connection', ws => {
+    console.log('Client connected via WebSocket');
+
+    client.on('message', (topic, message) => {
+        const data = message.toString();
+        ws.send(JSON.stringify({topic, data}));
+    });
+})
