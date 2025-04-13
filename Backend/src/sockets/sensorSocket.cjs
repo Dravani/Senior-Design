0938d888fd2b27bea388a27489f4a3226e895ef6
Sam Forstot <92872780+samforstot@users.noreply.github.com>
2025-04-13 18:10:52 -0400

    adjusts socket behavior

--- Backend/src/sockets/sensorSocket.cjs
+++ Backend/src/sockets/sensorSocket.cjs
@@ -8,7 +8,7 @@ const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY
 
 // EDIT THIS TOO, when we have multiple sensors beyond DHT and NETWORK 
 const setupWebSocket = (wsServer) => {
-  wsServer.on("connection", (ws, req) => {
+  wsServer.on("connection", async (ws, req) => {
     const sensorName = req.url.split("/")[2];
     console.log(`New WebSocket connection for sensor: ${sensorName}`);
 
@@ -18,9 +18,9 @@ const setupWebSocket = (wsServer) => {
     // Use appropriate Supabase client based on whether the request is for a network sensor
     const currentSupabaseClient = supabase;
     const tableName = isNetworkSensor ? "Network" : "Sensor";
-    const timeName = isNetworkSensor ? "created_at" : "timestamp";
+    // const timeName = isNetworkSensor ? "created_at" : "timestamp";
     const columVal = isNetworkSensor ? "ip" : "sensor_name";
-    const timeCheck = isNetworkSensor ? 300 : 2000;
+    // const timeCheck = isNetworkSensor ? 300 : 2000;
 
 
     // Function to send the latest sensor data
@@ -30,29 +30,44 @@ const setupWebSocket = (wsServer) => {
           .from(tableName)
           .select("*")
           .eq(columVal, sensorName)
-          .order(timeName, { ascending: false })
+          .order(isNetworkSensor ? "created_at" : "timestamp", { ascending: false })
           .limit(1)
           .single();
 
         if (error) {
           console.error("Database error:", error);
           ws.send(JSON.stringify({ error: "Failed to fetch data" }));
-          return;
+          // return;
+        } else {
+          // Send the latest data to the client
+          ws.send(JSON.stringify(data));
         }
-
-        // Send the latest data to the client
-        ws.send(JSON.stringify(data));
       } catch (error) {
         console.error("Error sending reading:", error);
       }
     };
 
-    // Send the latest reading every 2 seconds
-    const interval = setInterval(sendLatestReading, timeCheck);
+    const channel = currentSupabaseClient
+      .channel(`realtime:${tableName}-${sensorName}`)
+      .on(
+        "postgres_changes",
+        {
+          event:  "*",
+          schema: "public",
+          table: tableName,
+          filter: `${columVal}=eq.${sensorName}`
+        },
+        (payload) => {
+          console.log("Update: ", payload);
+          ws.send(JSON.stringify(payload.new));
+        }
+      )
+      .subscribe();
 
     // Cleanup when the WebSocket is closed
-    ws.on("close", () => {
-      clearInterval(interval);
+    ws.on("close", async () => {
+      //clearInterval(interval);
+      await currentSupabaseClient.removeChannel(channel);
       console.log(`WebSocket closed for sensor: ${sensorName}`);
     });
   });
